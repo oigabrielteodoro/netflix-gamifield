ac0d759a8cf9f1025e79017d4585327c050f9b16
Gabriel Teodoro <oi@gabrielteodoro.com>
2022-01-05 18:54:31 -0300

    Added config for theme and AppStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from 'react'
+
 import { Router } from 'router'
+import { AppStyle } from 'components'
 
 export function App() {
-  return <Router />
+  return (
+    <>
+      <Router />
+      <AppStyle />
+    </>
+  )
 }
---
+++ src/config/theme.ts
@@ -0,0 +1,18 @@
+export const theme = {
+  font: {
+    family: 'Barlow',
+    sizes: {
+      disclaimer: '1.2rem',
+      small: '1.4rem',
+      paragraph: '1.6rem',
+      subtitle: '2.2rem',
+      title: '3.2rem',
+    },
+  },
+  colors: {
+    neutral: {
+      800: '#191b1f',
+      900: '#09090a',
+    },
+  },
+}
